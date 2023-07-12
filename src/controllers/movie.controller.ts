84d798ff590d78a2a8bc68c335b77de58d85a1a5
avganka <karambasterg@gmail.com>
2023-07-13 01:45:30 +0300

    add search by person id

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -4,3 +4,10 @@ import {IMovie} from '../types/movie.types';
 export const getMovie = async (movieModel: Model<IMovie>, id: string): Promise<IMovie | null> => {
   return movieModel.findById(id);
 };
+
+export const getMoviesByPersonId = async (
+  movieModel: Model<IMovie>,
+  id: string
+): Promise<IMovie[] | null> => {
+  return movieModel.find({'persons.id': id});
+};
--- src/routes/movie.router.ts
+++ src/routes/movie.router.ts
@@ -1,12 +1,18 @@
-import {getMovie} from '../controllers/movie.controller';
+import {getMovie, getMoviesByPersonId} from '../controllers/movie.controller';
 import express from 'express';
 import Movie from '../models/movie.model';
+import movieModel from '../models/movie.model';
 
 const router = express.Router();
 
 router.get('/:id', async (req, res) => {
-  const film = await getMovie(Movie, req.params.id);
-  res.json(film);
+  const movie = await getMovie(Movie, req.params.id);
+  res.json(movie);
+});
+
+router.get('/byPersonId/:id', async (req, res) => {
+  const movies = await getMoviesByPersonId(Movie, req.params.id);
+  res.json({length: movies ? movies.length : 0, movie: movies});
 });
 
 export default router;
