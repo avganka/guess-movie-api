8ddea168a326390e049d78467dea1224386d4a46
avganka <karambasterg@gmail.com>
2023-07-12 22:47:08 +0300

    connect to db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
+
+mongoose
+  .connect(
+    `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}:27017/?authMechanism=DEFAULT`
+  )
+  .then(() => console.log('Connected to DB!!!!!'));
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
